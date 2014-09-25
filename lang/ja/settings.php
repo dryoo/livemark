13b96ad03875a3ec03691d27b2974231e0ff61b6
Hideaki SAWADA <chuno@live.jp>
2014-09-25 19:27:28 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+
+$lang['size'] = "透かしの幅（％）";
+$lang['watermark_path'] = '透かし画像（PNG）のパス。例：../plugins/livemark/livemark.png';
+$lang['minsize'] = "透かしを入れる画像の最小サイズ（ピクセル）";
+$lang['vertical'] = '透かしの垂直位置（％）';
+$lang['horizontal'] = '透かしの水平位置（％）';
+$lang['jpg_quality'] = "JPG 画像出力品質（0-100）";
