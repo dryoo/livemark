d4c8279f93f4790efabd838dbfa593c2caa3fc1e
dryoo <dryoo@live.com>
2014-09-25 17:08:48 +0900

    korean lang

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -1,4 +1,8 @@
 <?php
 
-$lang['size'] = "Size of the watermark (%)";
-$lang['minsize'] = "minimum size of image to be watermarked (pixel)";
+$lang['size'] = "워터마크 크기(넓이 %)";
+$lang['watermark_path'] = '워터마크 그림 경로 eg. ../plugins/livemark/livemark.png';
+$lang['minsize'] = "워터마크가 박힐 최소의 크기 (넓이 pixel) 이보다 작으면 안박힘";
+$lang['vertical'] = '세로 위치 (%) 가운데면 50';
+$lang['horizontal'] = '가로 위키 (%) 가운데면 50';
+$lang['jpg_quality'] = "출력 Jpg 품질 (0-100)";
